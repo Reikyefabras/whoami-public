229091e7ff5b69a2bcad31e16942add4fb391c3e
Reikyefabras <[rikiefabras@gmail.com]>
2025-04-10 02:58:36 +0800

    ini tugas js pertama

---
+++ reki.js
@@ -0,0 +1,12 @@
+const profile = {
+    nama: "Reiky Efabras Wahyu Wijaya",
+    alamat: "busur",
+    instagram: "@reikyefabras",
+    umur:"17"
+  };
+  
+  console.log("=== Profil ===");
+  console.log(`Nama: ${profile.nama}`);
+  console.log(`Alamat: ${profile.alamat}`);
+  console.log(`Instagram: ${profile.instagram}`);
+  console.log(`Umur:${profile.umur}`);
